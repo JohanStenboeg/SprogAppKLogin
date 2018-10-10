b1ded080c21dc2c6810cb0b84ad3e85b66d04000
Johan Stenboeg <joha6163@edu.easj.dk>
2018-10-10 14:07:15 +0200

    First commit

---
+++ login.js
@@ -0,0 +1,17 @@
+function onClickLogIn(){
+    var eMail = "admin";
+    var pass = "admin";
+    var typedInMail = document.getElementById("typedInEmail");
+    var typedInPass = document.getElementById("typedInPass");
+    if(eMail == typedInMail && pass == typedInPass){
+        alert("Du er nu logget ind");
+        
+        
+
+
+    } else{
+        alert("Ingen bruger med dine oplysninger");
+    }
+
+}
+
